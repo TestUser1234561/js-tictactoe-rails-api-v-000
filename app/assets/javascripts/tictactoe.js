13485b7d6d5ad7a2eed3d491f9290615ebe36266
Morgan Fudge <mfudge92@gmail.com>
2018-05-06 09:25:25 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,6 +1,18 @@
-
-
 $(document).ready(attachListeners)
+
+const win = [
+  [0, 1, 2]
+  [3, 4, 5]
+  [6, 7, 8]
+  [0, 3, 6]
+  [1, 4, 7]
+  [2, 5, 8]
+  [0, 4, 8]
+  [6, 4, 2]
+];
+let turn = 0;
+let currentGame = 0;
+
 
 function attachListeners() {
   $('td').click((target) => { if(!$(target).text && !checkWinner()) { doTurn(target) } })
