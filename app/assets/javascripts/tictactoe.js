7d8a60e63aca4b9d61d8993bccec19e443f2d822
Morgan Fudge <mfudge92@gmail.com>
2018-05-06 09:31:11 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,18 +1,24 @@
 $(document).ready(attachListeners)
 
-const win = [
+const WIN_COMBOS = [
   [0, 1, 2], [0, 3, 6], [0, 4, 8],
   [3, 4, 5], [1, 4, 7], [6, 4, 2],
   [6, 7, 8], [2, 5, 8]
 ];
 let turn = 0;
-let currentGame = 0;
+let game = 0;
 
 function attachListeners() {
   $('td').click((target) => { if(!$(target).text && !checkWinner()) { doTurn(target) } })
   $('#save').click(() => { saveGame() })
   $('#previous').click(() => { showPreviousGames() })
-  $('#clear').click(() => { saveGame() })
+  $('#clear').click(() => { resetBoard() })
 }
 
 function player() { turn % 2 ? 'O' : 'X' }
+
+function resetBoard() {
+  $('td').empty();
+  turn = 0;
+  game = 0;
+}
