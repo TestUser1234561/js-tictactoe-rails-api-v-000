383a867842d7b4f43bfb791042839c83e981dfcd
Morgan Fudge <mfudge92@gmail.com>
2018-05-06 10:20:22 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -71,7 +71,7 @@ function saveGame() {
   } else {
     console.log('2')
     $.post('/games', {state: board}, (data) => {
-      console.log(game)
+      console.log(data)
       game = game.data.id;
       //$('#games').append(`<button id="gameid-${game.data.id}">${game.data.id}</button><br>`);
       //$("#gameid-" + game.data.id).on('click', () => { loadGame(game.data.id) });
