f38c49dc1d715d45855e2fe60728576f943f0cc3
Morgan Fudge <mfudge92@gmail.com>
2018-05-06 09:48:00 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -15,7 +15,7 @@ function attachListeners() {
   $('#clear').click(() => { resetBoard() })
 }
 
-function player() { turn % 2 ? 'O' : 'X' }
+function player() { retun turn % 2 ? 'O' : 'X' }
 
 function doTurn(s) {
   console.log(player())
